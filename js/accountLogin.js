0226c97aaccc18ff299f3d277cb273ba1b6d5016
CavaBlan <xiaoyouwen00@yahoo.com>
2024-07-03 01:48:13 -0500

    Add promise in log in function

--- js/accountLogin.js
+++ js/accountLogin.js
@@ -4,13 +4,28 @@ const logInBtn = document.getElementById("log-in-btn");
 
 logInBtn.addEventListener("click", function(){
     alert(`Account: ${accInput.value}\nPassword: ${pwdInput.value}`);
+
+    // POST to backend servise
+    // const loginProm = new Promise((resolve, reject) =>{
+    //     const xhr = XMLHttpRequest();
+    //     xhr.open('POST', 'URL');
+    //     xhr.addEventListener('loadend', () => {
+    //         //TO-DO
+    //     });
+    //     xhr.setRequsetHeader('Content-Type', 'application/json');
+    //     const userObj = {
+    //         username : accInput.value,
+    //         password : pwdInput.value
+    //     }
+    //     const userStr = JSON.stringify(userObj);
+    //     xhr.send(userStr);
+    // })
+    // loginProm.then(result => {
+    //     //TO-DO
+    //     alert(`Account: ${accInput.value}\nPassword: ${pwdInput.value}`);
+    // }).catch(error =>{
+    //     console.dir(error);
+    // });
 })
 
-// POST to backend
-// axios({
-//     URL: '',
-//     method: 'POST',
-//     data: {}
-// }).then(result => {
 
-// })
